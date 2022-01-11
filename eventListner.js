ceb41479f2789b6439eee5836b690de03a0b6d1c
minseo <kim-west00@naver.com>
2022-01-11 23:31:43 +0900

    eventListner

---
+++ eventListner.js
@@ -0,0 +1,16 @@
+﻿const h1 = document.querySelector("div.hello:first-child h1")
+
+function handleTitleClick(){
+    h1.style.color = "blue";
+}
+function handleMouseEnter(){
+    h1.innerText = "Mouse is here";
+}
+function handleWindowResize(){
+    document.body.style.backgroundColor = "tomato";
+}
+
+
+h1.addEventListener("click", handleTitleClick);
+h1.addEventListener("mouseenter", handleMouseEnter);
+h1.addEventListener("resize", handleWindowResize);
