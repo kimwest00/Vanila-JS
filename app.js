7afd62cd1b92736cddfbed192aa34ccb83a69890
minseo <kim-west00@naver.com>
2022-01-12 22:06:04 +0900

    form태그안에 작성하면 html 구현가능;form태그로 제출된 값을 불려오려면?

--- app.js
+++ app.js
@@ -3,17 +3,6 @@ const loginForm = document.getElementById('login-form');
 const loginInput = loginForm.querySelector("input");
 const loginButton = loginForm.querySelector("button");
 function onLoginBtnClick(){
-    //loginInput창을 통해 입력한 값이 어떻게 저장되는지 보기위한 코드라인
-    console.dir(loginInput);//loginInput.value로 사용하면됌
-    const value = loginInput.value;
-    if(value.length >= 15){
-        alert("Please shorten your name;too long");
-    }
-    else if(value){
-        console.log("hello",value);
-    }
-    else{
-        alert("Please write your name");
-    }
+    console.log(loginInput.value)
 }
-loginButton.addEventListener("click",onLoginBtnClick);
+loginForm.addEventListener("click",onLoginBtnClick);
