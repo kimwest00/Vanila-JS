c2df9864366f6113d5e107b4c217984954bb386c
minseo <kim-west00@naver.com>
2022-01-12 20:55:01 +0900

    css in js

--- eventListner.js
+++ eventListner.js
@@ -1,16 +1,24 @@
 ﻿const h1 = document.querySelector("div.hello:first-child h1")
 
 function handleTitleClick(){
-    h1.style.color = "blue";
+    const currentColor = h1.style.color;
+    let newColor;
+    if (currentColor === "blue"){
+        newColor = "tomato";
+    }else{
+        newColor = "blue";
+    }
+    h1.style.color = newColor;
 }
-function handleMouseEnter(){
+
+/*function handleMouseEnter(){
     h1.innerText = "Mouse is here";
 }
 function handleWindowResize(){
     document.body.style.backgroundColor = "tomato";
-}
+}*/
 
 
 h1.addEventListener("click", handleTitleClick);
-h1.addEventListener("mouseenter", handleMouseEnter);
-h1.addEventListener("resize", handleWindowResize);
+//h1.addEventListener("mouseenter", handleMouseEnter);
+//h1.addEventListener("resize", handleWindowResize);
