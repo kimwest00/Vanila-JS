72483a9eaaf548574347dcd0d70886433ab36589
minseo <kim-west00@naver.com>
2022-01-12 21:23:36 +0900

    방법3+)toggle 사용

--- eventListner.js
+++ eventListner.js
@@ -1,16 +1,8 @@
 ﻿const h1 = document.querySelector("div.hello:first-child h1")
 
 function handleTitleClick(){
-    //"active"자체로 비교하면 에러의 우려가 많다
-    //만약 h1에게 className이 있었다면??
-    //해당코드를 실행하면 className이 사라진다
-    //classList를 사용해보자
-    const clickedClass = "active";
-    if(h1.classList.contains(clickedClass)){
-        h1.classList.remove(clickedClass);
-    }else{
-        h1.classList.add(clickedClass);
-    }
+    //classList의 내장 속성 toggle로 구현가능 
+    h1.classList.toggle("active");
     
 }
 
