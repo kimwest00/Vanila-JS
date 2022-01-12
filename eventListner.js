91bbdba74a96f63b2a8dc01149b5065c31870cdd
minseo <kim-west00@naver.com>
2022-01-12 20:59:30 +0900

    방법2)className변경으로 색깔변경

--- eventListner.js
+++ eventListner.js
@@ -1,7 +1,12 @@
 ﻿const h1 = document.querySelector("div.hello:first-child h1")
 
 function handleTitleClick(){
-    h1.className = "active";
+    if(h1.className === "active"){
+        h1.className = "";
+    }else{
+        h1.className = "active";
+    }
+    
 }
 
 /*function handleMouseEnter(){
