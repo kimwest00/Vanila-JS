d023f30ca0125593176052ad17decb638f260caa
minseo <kim-west00@naver.com>
2022-01-12 21:57:19 +0900

    value 값에 따른 출력 + class->id로 수정

--- app.js
+++ app.js
@@ -4,7 +4,16 @@ const loginInput = loginForm.querySelector("input");
 const loginButton = loginForm.querySelector("button");
 function onLoginBtnClick(){
     //loginInput창을 통해 입력한 값이 어떻게 저장되는지 보기위한 코드라인
-    console.dir(loginInput);
-    console.log("click");
+    console.dir(loginInput);//loginInput.value로 사용하면됌
+    const value = loginInput.value;
+    if(value.length >= 15){
+        alert("Please shorten your name;too long");
+    }
+    else if(value){
+        console.log("hello",value);
+    }
+    else{
+        alert("Please write your name");
+    }
 }
 loginButton.addEventListener("click",onLoginBtnClick);
