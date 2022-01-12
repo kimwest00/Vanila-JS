0914cb9f1b05c5449c5d9b5ac1b9087ed362e5b8
minseo <kim-west00@naver.com>
2022-01-12 21:14:13 +0900

    방법3)classList 사용하기_원래 className이 있다면?

--- eventListner.js
+++ eventListner.js
@@ -1,10 +1,15 @@
 ﻿const h1 = document.querySelector("div.hello:first-child h1")
 
 function handleTitleClick(){
-    if(h1.className === "active"){
-        h1.className = "";
+    //"active"자체로 비교하면 에러의 우려가 많다
+    //만약 h1에게 className이 있었다면??
+    //해당코드를 실행하면 className이 사라진다
+    //classList를 사용해보자
+    const clickedClass = "active";
+    if(h1.classList.contains(clickedClass)){
+        h1.classList.remove(clickedClass);
     }else{
-        h1.className = "active";
+        h1.classList.add(clickedClass);
     }
     
 }
