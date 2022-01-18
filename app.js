a416bd0dcc5db42ba265e6e4f2e1b316f7dbfa1f
minseo <kim-west00@naver.com>
2022-01-18 22:15:04 +0900

    preventDefault 함수로 새로고침이 되지않아 console.log가 작동된다

--- app.js
+++ app.js
@@ -2,9 +2,9 @@
 const loginForm = document.getElementById('login-form');
 const loginInput = loginForm.querySelector("input");
 const loginButton = loginForm.querySelector("button");
-function onLoginSubmit(inform){
-    inform.preventDefault();
-    console.log(inform);//onLoginSubmit 함수에 대한 인자로 값을 전달받고있다
+function onLoginSubmit(event){
+    event.preventDefault();//기본동작인 브라우저 새로고침을 막고있다
+    console.log(loginInput.value);//onLoginSubmit 함수에 대한 인자로 값을 전달받고있다
 }
 loginForm.addEventListener("submit",onLoginSubmit);
 document.getElement
