d02157b169aa5e5416bb7fdc5d456a13b91528df
minseo <kim-west00@naver.com>
2022-01-18 23:10:47 +0900

    +h1은 hidden class remove로 표출

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ const loginForm = document.getElementById('login-form');
 const loginInput = loginForm.querySelector("input");
 const loginButton = loginForm.querySelector("button");
 const link = document.querySelector("a");
+const greeting = document.querySelector("#greeting");
 function onLoginSubmit(event){
     event.preventDefault();//기본동작인 브라우저 새로고침을 막고있다
     const username = loginInput.value;//onLoginSubmit 함수에 대한 인자로 값을 전달받고있다
     loginForm.classList.add("hidden");
     console.log(username);
+    greeting.innerText = "Hello" + username;
+    greeting.classList.remove("hidden");
 }
 function handleLink(event){
     alert("prevent page until click OK");
