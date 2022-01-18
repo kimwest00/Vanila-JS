1541955352825504d5db2dfab221c915538ade3e
minseo <kim-west00@naver.com>
2022-01-18 23:45:11 +0900

    savedUsername에 따른 변화+paintGreetings 함수로 greeting 문구 만들기

--- app.js
+++ app.js
@@ -4,23 +4,32 @@ const loginInput = loginForm.querySelector("input");
 const loginButton = loginForm.querySelector("button");
 const link = document.querySelector("a");
 const greeting = document.querySelector("#greeting");
+//반복되는 변수는 오타의 위험성이 있으므로, 변수로만들어준다
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 function onLoginSubmit(event){
     event.preventDefault();//기본동작인 브라우저 새로고침을 막고있다
+    loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;//onLoginSubmit 함수에 대한 인자로 값을 전달받고있다
-    loginForm.classList.add("hidden");
     localStorage.setItem("username",username);
+    paintGreetings(username);
+    
+}
+function paintGreetings(username){
     greeting.innerText = `Hello ${username}`;//`(백틱)를 이용한 띄어쓰기 추가
-    greeting.classList.remove("hidden");
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
-function handleLink(event){
-    alert("prevent page until click OK");
-}
-loginForm.addEventListener("submit",onLoginSubmit);
-link.addEventListener("click",handleLink);
-const savedUsername = localStorage.getItem("username");
+const savedUsername = localStorage.getItem(USERNAME_KEY);
 if(savedUsername === null){
     //아직 입력되지않았기때문에 form을 보여주자
-    
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit",onLoginSubmit);
 }else{
     //입력되었기때문에 greetings(h1)을 보여주자
+    paintGreetings(savedUsername);
+}
+
+link.addEventListener("click",handleLink);
+function handleLink(event){
+    alert("prevent page until click OK");
 }
