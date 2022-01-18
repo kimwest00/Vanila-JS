57d865abe8ffc22cb4a6cfb18a228199ac2cb871
minseo <kim-west00@naver.com>
2022-01-18 22:54:49 +0900

    alert가 링크연결을 잠시동안 막는다

--- app.js
+++ app.js
@@ -2,9 +2,15 @@
 const loginForm = document.getElementById('login-form');
 const loginInput = loginForm.querySelector("input");
 const loginButton = loginForm.querySelector("button");
+const link = document.querySelector("a");
 function onLoginSubmit(event){
     event.preventDefault();//기본동작인 브라우저 새로고침을 막고있다
     console.log(loginInput.value);//onLoginSubmit 함수에 대한 인자로 값을 전달받고있다
 }
+function handleLink(event){
+    alert("prevent page until click OK")
+}
 loginForm.addEventListener("submit",onLoginSubmit);
+link.addEventListener("click",handleLink);
+
 document.getElement
